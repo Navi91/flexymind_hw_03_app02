eeadba60ee87e1585f4b7840aa8c44829161a626
Navi91 <navi91@inbox.ru>
2013-04-12 21:04:01 +0400

    Закоммичены только необходимые файлы

---
+++ App02/src/com/example/App02/MyActivity.java
@@ -0,0 +1,57 @@
+package com.example.App02;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void onChangePosition(View view) {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.topMargin = (int) (Math.random() * 1000) % 210;
+        layoutParams.leftMargin = (int) (Math.random() * 1000) % 180;
+
+        view.setLayoutParams(layoutParams);
+    }
+
+
+}
